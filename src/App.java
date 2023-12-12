ca8a65c83233547186c493b776f6471f7f177441
Kennedy Alves <kennedy.alves@csjsistemas.com.br>
2023-12-12 12:08:13 -0300

    changes to the menu structure

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-import static menus.MainMenu.mainMenu;
-import static questions.introductionToJava.Q01.IntroductionToJavaQ1.introductionToJava01;
-import static menus.IntroductionToJavaMenu.introductionToJavaMenu;
+import static menus.Menus.cleanScreen;
+import static menus.Menus.mainMenu;
+import static questions.introductionToJava.IntroductionToJavaMain.introductionToJavaMain;
 
 public class App {
     public static void main(String[] args) {
-        int listChoose, questionChoose;
+        int listChoose;
         Scanner input = new Scanner(System.in);
 
         do{
@@ -16,39 +16,19 @@ public class App {
             cleanScreen();
 
             switch (listChoose){
-                case 1 -> {
-                    cleanScreen();
-                    introductionToJavaMenu();
-                    questionChoose= Integer.parseInt(input.nextLine());
-                    cleanScreen();
-
-                    switch (questionChoose) {
-                        case 1 -> introductionToJava01();
-                        case 0 ->{}
-                        default-> System.out.println("Opção inválida!*");
-                    }
-                }
+                case 1 -> introductionToJavaMain();
 
                 case 2 -> System.out.println();
 
                 case 8 -> System.out.println("Saindo...");
+
+                case 0 -> System.out.println("Saindo...");
                 
                 default -> System.out.println("Opção inválida!*");
             }
-        }while (listChoose != 8 );
+        }while (listChoose != 0 );
         input.close();
     }
 
-    private static void cleanScreen(){
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-            }
-        } catch (Exception e) {
-            System.err.println("Erro ao limpar o terminal: " + e.getMessage());
-        }
-    }
+  
 }
---
+++ src/questions/introductionToJava/IntroductionToJavaMain.java
@@ -0,0 +1,48 @@
+package questions.introductionToJava;
+
+import java.util.Scanner;
+
+import static menus.Menus.introductionToJavaMenu;
+import static menus.Menus.cleanScreen;
+import static questions.introductionToJava.Q01.IntroductionToJavaQ1.introductionToJava01;
+
+
+public class IntroductionToJavaMain {
+    
+    public static void introductionToJavaMain(){
+        Scanner input = new Scanner(System.in);
+        int questionChoose;
+        do{
+            cleanScreen();
+            introductionToJavaMenu();
+            questionChoose = input.nextInt();
+            cleanScreen();
+            switch (questionChoose){
+                case 1 -> introductionToJava01();
+                case 2 -> System.out.println();
+                case 3 -> System.out.println();
+                case 4 -> System.out.println();
+                case 5 -> System.out.println();
+                case 6 -> System.out.println();
+                case 7 -> System.out.println();
+                case 8 -> System.out.println();
+                case 9 -> System.out.println();
+                case 10 -> System.out.println();
+                case 11 -> System.out.println();
+                case 12 -> System.out.println();
+                case 13 -> System.out.println();
+                case 14 -> System.out.println();
+                case 15 -> System.out.println();
+                case 16 -> System.out.println();
+                case 17 -> System.out.println();
+                case 18 -> System.out.println();
+                case 19 -> System.out.println();
+                case 20 -> System.out.println();  
+                case 21 -> System.out.println();             
+                case 22 -> System.out.println();
+                case 0 ->{}
+                default -> System.out.println("Opção inválida!*");
+            }
+        }while (questionChoose != 0);
+    }
+}
